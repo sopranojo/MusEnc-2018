0ce5c816b6e2dc91371bac51fb0498f5384e3bd5
sopranojo <sopranojo@gmail.com>
2018-06-15 11:04:55 -0700

    work done for DHSI class!

--- js/index.js
+++ js/index.js
@@ -20,4 +20,4 @@ options = {
 vrvToolkit.setOptions(options)
 
 /* Show the score */
-showScore('score', 'data/example.mei') // <-- CHANGE THIS TO YOUR FILE
+showScore('score', 'data/clara.mei') // <-- CHANGE THIS TO YOUR FILE
